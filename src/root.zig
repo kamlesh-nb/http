e02ab17ed0cd1b804b1b830d73c5faa15c7627d6
kamlesh-nb <kamlesh.bambarde@outlook.com>
2024-01-24 09:49:16 +0530

    ....

--- src/root.zig
+++ src/root.zig
@@ -2,4 +2,5 @@ pub const Request = @import("lib/request.zig");
 pub const Response = @import("lib/response.zig");
 pub const Method = @import("lib/method.zig").Method;
 pub const Status = @import("lib/status.zig").Status;
+pub const Version = @import("lib/version.zig").Version;
 pub const Buffer = @import("buffer").Buffer;
