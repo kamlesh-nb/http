1668b8e52f4118082b3bf588ec26d0fac6fc0057
kamlesh-nb <kamlesh.bambarde@outlook.com>
2025-04-23 20:11:15 +0530

    mime added

--- src/root.zig
+++ src/root.zig
@@ -3,3 +3,4 @@ pub const Response = @import("lib/response.zig");
 pub const Method = @import("lib/method.zig").Method;
 pub const Status = @import("lib/status.zig").Status;
 pub const Version = @import("lib/version.zig").Version;
+pub const Mime = @import("lib/mime.zig").Mime;
